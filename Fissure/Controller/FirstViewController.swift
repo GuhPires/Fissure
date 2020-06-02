8a34ee12d2f199dde8e46e2c97b8f1d2b630a141
Gustavo <gustavo.goncalvespires@gmail.com>
2020-06-02 19:48:52 -0300

    Setup + Initial commit

---
+++ Fissure/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Fissure
+//
+//  Created by Gustavo Mac Mini on 02/06/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fissure/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Fissure
+//
+//  Created by Gustavo Mac Mini on 02/06/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
